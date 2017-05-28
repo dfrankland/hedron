97e74599defc730b31b8476b60b9894babfd0816
Dylan Frankland <dylan@frankland.io>
2017-05-27 22:31:56 -0700

    add missing react import

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,4 +1,5 @@
 // @flow
+import React from "react";
 import styled from "styled-components";
 import { withLayout } from "./LayoutProvider";
 import { breakpoint, applyBreakpointStyle } from "../utils";
